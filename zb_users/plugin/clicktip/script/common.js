9e9d0d5838003ca687389f98158bf9983c5648a7
chelflan <284369904@qq.com>
2019-10-14 09:50:46 +0800

    修改样式

--- zb_users/plugin/clicktip/script/common.js
+++ zb_users/plugin/clicktip/script/common.js
@@ -1,7 +1,7 @@
 $(function(){
 	var keyindex = "0";
 	$("body").click(function(e){
-		var key = new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善");
+		var key = new Array("💐","🌸","💮","🌹","🌺","🌻","🌼","🌷","🌱","🌲","🌳","🌴","🌵","🌾","🌿","🍀","🍁","🍂","🍃");
 		var $i = $("<span/>").text(key[keyindex]);
 		keyindex = (keyindex + 1) % key.length;
 		var x = e.pageX, y = e.pageY;
@@ -10,7 +10,7 @@ $(function(){
 			"top":y - 20,
 			"left":x,
 			"position":"absolute",
-			"font-size":"14px",
+			"font-size":"20px",
 			"color":"#004C98",
 			"font-family":"microsoft yahei",
 			"font-style":"normal"
